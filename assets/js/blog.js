66676c9ccfb1c0f2eb9e75a66164338b811740c8
jatinsinghnp <js8971105@gmail.com>
2021-08-04 08:50:04 +0545

    aaba yo danda edit twa teatro ni hudaie na

---
+++ assets/js/blog.js
@@ -0,0 +1,11 @@
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+
+alert('hellow wor')
